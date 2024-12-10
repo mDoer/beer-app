383309b1f4a1b0036baf7143674c8af7548f2d78
Standard <maximilian.doernbrack@gmx.de>
2024-12-10 22:12:14 +0100

    more tomcat stuff

---
+++ src/main/java/mdoer/springframework/spring6restmvc/BeerApp.java
@@ -0,0 +1,14 @@
+package mdoer.springframework.spring6restmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class BeerApp extends SpringBootServletInitializer {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(BeerApp.class, args);
+	}
+
+}
